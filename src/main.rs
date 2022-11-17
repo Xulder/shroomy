3743a5ec7abeacd37c7283de19a1f2d3fcd39635
Xulder <sebastianjansen04@gmail.com>
2022-11-16 23:00:45 -0800

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,73 @@
+use bevy::{
+    prelude::Plugin,
+    prelude::*,
+    render::{camera::ScalingMode, texture::ImageSettings},
+};
+
+use bevy_inspector_egui::WorldInspectorPlugin;
+
+pub mod debug;
+pub mod player;
+
+// use debug::DebugPlugin;
+use player::PlayerPlugin;
+
+pub const CLEAR: Color = Color::rgb(0.1, 0.1, 0.1);
+pub const RESOLUTION: f32 = 16.0 / 9.0;
+pub const TILE_SIZE: f32 = 0.3;
+
+fn main() {
+    App::new()
+        .insert_resource(ClearColor(CLEAR))
+        .insert_resource(WindowDescriptor {
+            width: 1600.0,
+            height: 900.0,
+            title: "Shroomy".to_string(),
+            resizable: false,
+            ..Default::default()
+        })
+        .insert_resource(ImageSettings::default_nearest())
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(spawn_camera)
+        .add_plugin(PlayerPlugin)
+        .add_plugin(WorldInspectorPlugin::new())
+        // .add_plugin(DebugPlugin)
+        .add_startup_system_to_stage(StartupStage::PreStartup, load_spritesheet)
+        .run();
+}
+
+fn spawn_camera(mut commands: Commands) {
+    let mut camera = Camera2dBundle::default();
+
+    camera.projection.top = 1.0;
+    camera.projection.bottom = -1.0;
+
+    camera.projection.right = 1.0 * RESOLUTION;
+    camera.projection.left = -1.0 * RESOLUTION;
+
+    camera.projection.scaling_mode = ScalingMode::None;
+
+    commands.spawn_bundle(camera);
+}
+
+struct SpriteSheet(Handle<TextureAtlas>);
+
+fn load_spritesheet(
+    mut commands: Commands,
+    assets: Res<AssetServer>,
+    mut texture_atlases: ResMut<Assets<TextureAtlas>>,
+) {
+    let image = assets.load("devart/shroomy-ph.png");
+    let atlas = TextureAtlas::from_grid_with_padding(
+        image,
+        Vec2::splat(32.0),
+        1,
+        1,
+        Vec2::splat(2.0),
+        Vec2::splat(0.0),
+    );
+
+    let atlas_handle = texture_atlases.add(atlas);
+
+    commands.insert_resource(SpriteSheet(atlas_handle));
+}
---
+++ src/player.rs
@@ -0,0 +1,58 @@
+use bevy::prelude::*;
+
+use crate::{SpriteSheet, TILE_SIZE};
+
+pub struct PlayerPlugin;
+
+#[derive(Component)]
+pub struct Player {
+    speed: f32,
+}
+
+impl Plugin for PlayerPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_startup_system(spawn_player)
+            .add_system(player_movement);
+    }
+}
+
+fn player_movement(
+    mut player_query: Query<(&Player, &mut Transform)>,
+    keyboard: Res<Input<KeyCode>>,
+    time: Res<Time>,
+) {
+    let (player, mut transform) = player_query.single_mut();
+
+    if keyboard.pressed(KeyCode::W) {
+        transform.translation.y += player.speed * time.delta_seconds();
+    }
+    if keyboard.pressed(KeyCode::S) {
+        transform.translation.y -= player.speed * time.delta_seconds();
+    }
+    if keyboard.pressed(KeyCode::A) {
+        transform.translation.x -= player.speed * time.delta_seconds();
+    }
+    if keyboard.pressed(KeyCode::D) {
+        transform.translation.x += player.speed * time.delta_seconds();
+    }
+}
+
+fn spawn_player(mut commands: Commands, spritesheet: Res<SpriteSheet>) {
+    let mut sprite = TextureAtlasSprite::new(0);
+    sprite.color = Color::rgb(1.0, 1.0, 1.0);
+    sprite.custom_size = Some(Vec2::splat(TILE_SIZE));
+
+    commands
+        .spawn_bundle(SpriteSheetBundle {
+            sprite,
+            texture_atlas: spritesheet.0.clone(),
+            transform: Transform {
+                translation: Vec3::new(0.0, 0.0, 900.0),
+                ..Default::default()
+            },
+            ..Default::default()
+        })
+        .insert(Name::new("Player"))
+        .insert(Player { speed: 1.0 })
+        .id();
+}
